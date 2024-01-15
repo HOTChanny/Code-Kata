5e50018a696fa99646e1b294614e23afca43486f
김하챤 <dbtldk12@naver.com>
2024-01-15 09:54:18 +0900

    [level Lv.0] Title: 숫자 비교하기, Time: 0.16 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer;
+        if(num1==num2){
+            answer = 1;
+            return answer;
+        }
+        else{
+            answer = -1;
+            return answer;
+        }
+    }
+}
